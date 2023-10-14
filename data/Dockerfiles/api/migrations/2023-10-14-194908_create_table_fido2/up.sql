f4d578d8938d8a582a730137338821ca975159d8
Elis Odenhage <teknikgillaren@gmail.com>
2023-10-14 22:44:38 +0200

    add id as primary key for fido2 table

--- data/Dockerfiles/api/migrations/2023-10-14-194908_create_table_fido2/up.sql
+++ data/Dockerfiles/api/migrations/2023-10-14-194908_create_table_fido2/up.sql
@@ -1,5 +1,6 @@
 -- Your SQL goes here
 CREATE TABLE fido2(
+  `id` INTEGER AUTO_INCREMENT NOT NULL,
   `username` VARCHAR(255) NOT NULL,
   `friendlyName` VARCHAR(255),
   `rpId` VARCHAR(255) NOT NULL,
@@ -14,5 +15,5 @@ CREATE TABLE fido2(
   `created` DATETIME(0) NOT NULL DEFAULT NOW(0),
   `modified` DATETIME ON UPDATE NOW(0),
   `active` TINYINT(1) NOT NULL DEFAULT '1',
-  primary key(`rpId`)
+  primary key(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC
