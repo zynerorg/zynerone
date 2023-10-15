7dd73dfc98b1158a08f5b900115888cab6380999
Vermium Sifell <vermium@zyner.org>
2023-10-15 14:34:29 +0200

    migration for sieve_after

---
+++ data/Dockerfiles/api/migrations/2023-10-15-120518_create_view_sieve_after/down.sql
@@ -0,0 +1,7 @@
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `sieve_after`
+--
+DROP TABLE IF EXISTS `sieve_after`;
---
+++ data/Dockerfiles/api/migrations/2023-10-15-120518_create_view_sieve_after/up.sql
@@ -0,0 +1,7 @@
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `sieve_after`
+--
+CREATE ALGORITHM=UNDEFINED DEFINER=`mailcow`@`%` SQL SECURITY DEFINER VIEW `sieve_after`  AS SELECT md5(`sieve_filters`.`script_data`) AS `id`, `sieve_filters`.`username` AS `username`, `sieve_filters`.`script_name` AS `script_name`, `sieve_filters`.`script_data` AS `script_data` FROM `sieve_filters` WHERE `sieve_filters`.`filter_type` = 'postfilter' ;
