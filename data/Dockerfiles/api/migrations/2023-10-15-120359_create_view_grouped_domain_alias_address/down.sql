c31b3d6285880e60c81f538e618bf1eb1e2d8c52
Vermium Sifell <vermium@zyner.org>
2023-10-15 14:34:01 +0200

    migration for grouped_domain_alias_address

---
+++ data/Dockerfiles/api/migrations/2023-10-15-120359_create_view_grouped_domain_alias_address/down.sql
@@ -0,0 +1,7 @@
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `grouped_domain_alias_address`
+--
+DROP TABLE IF EXISTS `grouped_domain_alias_address`;
---
+++ data/Dockerfiles/api/migrations/2023-10-15-120359_create_view_grouped_domain_alias_address/up.sql
@@ -0,0 +1,7 @@
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `grouped_domain_alias_address`
+--
+CREATE ALGORITHM=UNDEFINED DEFINER=`mailcow`@`%` SQL SECURITY DEFINER VIEW `grouped_domain_alias_address`  AS SELECT `mailbox`.`username` AS `username`, ifnull(group_concat(`mailbox`.`local_part`,'@',`alias_domain`.`alias_domain` separator ' '),'') AS `ad_alias` FROM (`mailbox` left join `alias_domain` on(`alias_domain`.`target_domain` = `mailbox`.`domain`)) GROUP BY `mailbox`.`username` ;
