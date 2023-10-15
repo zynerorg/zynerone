53593a9dbca885f14c186cabc5972c9ad3ef7a88
Vermium Sifell <vermium@zyner.org>
2023-10-15 14:26:40 +0200

    get rid of old migrations

--- data/Dockerfiles/api/migrations/2023-10-14-192008_create_table_versions/up.sql
+++
@@ -1,7 +0,0 @@
--- Your SQL goes here
-CREATE TABLE versions(
-  `application` VARCHAR(255) NOT NULL,
-  `version` VARCHAR(100) NOT NULL,
-  `created` DATETIME(0) NOT NULL DEFAULT NOW(0),
-  primary key(`application`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
