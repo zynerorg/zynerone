2b894a5c5055ffc0152965860cd5e6c4f84115ad
Vermium Sifell <vermium@zyner.org>
2023-10-20 14:48:16 +0200

    improve rust code

--- data/Dockerfiles/api/src/main.rs
+++ data/Dockerfiles/api/src/main.rs
@@ -2,57 +2,37 @@ mod structs;
 
 use rocket::State;
 use structs::Data;
-use hyper::{body::HttpBody, Client};
+use hyper::{Client};
 use hyperlocal::{UnixClientExt, Uri};
-use tokio::io::{self, AsyncWriteExt as _};
-use tokio::io::AsyncRead;
 use std::error::Error;
-use tokio::io::BufStream;
-use std::any::Any;
-use rocket::response::content;
 use serde_json::Value;
-use serde_json::Map;
-use tokio::task;
 use futures::future;
 
 #[macro_use] extern crate rocket;
 
+const DOCKER_SOCKET: &str = "/var/run/docker.sock";
+
 async fn get_container_info(data: &State<Data>, container_id: &str) -> Result<Value, Box<dyn Error + Send>> {
-    
-    let url = Uri::new("/var/run/docker.sock", format!("/containers/{}/json", container_id).as_str()).into();   
+    let path = &format!("/containers/{}/json", container_id);
+    let uri = Uri::new(DOCKER_SOCKET, path).into();
+    let docker_response = data.docker.get(uri).await.unwrap();    
+    let body_bytes = hyper::body::to_bytes(docker_response.into_body()).await.unwrap();
 
-    let mut response = data.docker.get(url).await.unwrap();
-
-    let buf = hyper::body::to_bytes(response).await.unwrap();
-    let res_json_str = std::str::from_utf8(&buf).unwrap();
-
-    let res_data: Value = serde_json::from_str(res_json_str).unwrap();
-    Ok(res_data)
+    let response: Value = serde_json::from_slice(&body_bytes).unwrap();
+    Ok(response)
 }
 
 #[get("/api/v1/system/containers")]
 async fn v1_system_containers(data: &State<Data>) -> Value {
+    let path = "/containers/json";
+    let uri = Uri::new(DOCKER_SOCKET, path).into();   
+    let docker_response = data.docker.get(uri).await.unwrap();
+    let body_bytes = hyper::body::to_bytes(docker_response.into_body()).await.unwrap();
 
+    let response: Value = serde_json::from_slice(&body_bytes).unwrap();
 
-    let url = Uri::new("/var/run/docker.sock", "/containers/json").into();   
-
-    let mut response = data.docker.get(url).await.unwrap();
-
-    let buf = hyper::body::to_bytes(response).await.unwrap();
-    let res_json_str = std::str::from_utf8(&buf).unwrap();
-    // //let body: Any = serde_json::from_slice(&buf).unwrap();
-    // let res_json: Value = serde_json::from_str(res_json_str).unwrap();
-    // let res_json_arr = res_json.as_array();
-    // let res_json_object = Map::<String, Value>::new();
-    // for container in res_json_arr {
-    // 	res_json_object.insert(container.get("Id").unwrap().to_string(), serde_json::to_value(container).unwrap());
-    // }
-
-    // Deserialize the JSON data into a Value
-    let res_data: Value = serde_json::from_str(res_json_str).unwrap();
-
-    let container_json_array = future::try_join_all(
-	res_data
+    let containers_array = future::try_join_all(
+        response
 	    .as_array()
 	    .unwrap_or(&vec![])
 	    .iter()
@@ -69,8 +49,7 @@ async fn v1_system_containers(data: &State<Data>) -> Value {
 	.unwrap();
 
 
-    // Create a custom JSON object where each container ID maps to its corresponding container object
-    let custom_json_object = container_json_array
+    let containers_response = containers_array
         .iter()
         .map(|container| {
 
@@ -81,8 +60,8 @@ async fn v1_system_containers(data: &State<Data>) -> Value {
             (container_id.to_string(), container_info.clone())
         })
         .collect::<serde_json::Map<String, Value>>();
-    serde_json::to_value(custom_json_object).unwrap()
-// Create a vector of tasks to process each container asynchronously
+
+    serde_json::to_value(containers_response).unwrap()
 }
 
 #[launch]
