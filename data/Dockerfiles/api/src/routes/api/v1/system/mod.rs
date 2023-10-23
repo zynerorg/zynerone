f203f306ddf62d2075fd67f0efd1d03fa72b1d21
Elis Odenhage <teknikgillaren@gmail.com>
2023-10-23 16:50:44 +0200

    use macro for route urls

--- data/Dockerfiles/api/src/routes/api/v1/system/mod.rs
+++ data/Dockerfiles/api/src/routes/api/v1/system/mod.rs
@@ -2,7 +2,8 @@ pub mod containers;
 
 use rocket::Rocket;
 use rocket::Build;
+use crate::route_url;
 
 pub fn routes(rocket: Rocket<Build>, base_url: &str) -> Rocket<Build> {
-    containers::routes(rocket, &format!("{}/{}/", base_url, "system"))
+    containers::routes(rocket, route_url!(base_url, "/system"))
 }
--- data/Dockerfiles/api/src/routes/mod.rs
+++ data/Dockerfiles/api/src/routes/mod.rs
@@ -2,7 +2,8 @@ pub mod api;
 
 use rocket::Rocket;
 use rocket::Build;
+use crate::route_url;
 
 pub fn routes(rocket: Rocket<Build>, base_url: &str) -> Rocket<Build> {
-    api::routes(rocket, &format!("{}/", base_url))
+    api::routes(rocket, route_url!(base_url, "/"))
 }
