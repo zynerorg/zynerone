4a01b49ab0740788feed5cb9fe0427d1a058936f
Mastergamer433 <75840421+Mastergamer433@users.noreply.github.com>
2023-10-23 16:25:21 +0200

    refactor code (#34)
    * refactor code to multiple files
    
    * added main.cf file again
    
    * change main.cf file to upstream

--- data/Dockerfiles/api/src/main.rs
+++ data/Dockerfiles/api/src/main.rs
@@ -1,75 +1,25 @@
 mod structs;
+mod routes;
+mod helpers;
 
-use rocket::State;
+use rocket::{
+    Build,
+    Rocket,
+};
 use structs::Data;
 use hyper::{Client};
-use hyperlocal::{UnixClientExt, Uri};
-use std::error::Error;
-use serde_json::Value;
-use futures::future;
+use hyperlocal::{UnixClientExt};
 
 #[macro_use] extern crate rocket;
 
-const DOCKER_SOCKET: &str = "/var/run/docker.sock";
-
-async fn get_container_info(data: &State<Data>, container_id: &str) -> Result<Value, Box<dyn Error + Send>> {
-    let path = &format!("/containers/{}/json", container_id);
-    let uri = Uri::new(DOCKER_SOCKET, path).into();
-    let docker_response = data.docker.get(uri).await.unwrap();    
-    let body_bytes = hyper::body::to_bytes(docker_response.into_body()).await.unwrap();
-
-    let response: Value = serde_json::from_slice(&body_bytes).unwrap();
-    Ok(response)
-}
-
-#[get("/api/v1/system/containers")]
-async fn v1_system_containers(data: &State<Data>) -> Value {
-    let path = "/containers/json";
-    let uri = Uri::new(DOCKER_SOCKET, path).into();   
-    let docker_response = data.docker.get(uri).await.unwrap();
-    let body_bytes = hyper::body::to_bytes(docker_response.into_body()).await.unwrap();
-
-    let response: Value = serde_json::from_slice(&body_bytes).unwrap();
-
-    let containers_array = future::try_join_all(
-        response
-	    .as_array()
-	    .unwrap_or(&vec![])
-	    .iter()
-	    .map(|container| {
-
-		let container_id = container["Id"].as_str().unwrap_or_default();
-
-		let container_info = get_container_info(data, container_id);
-
-		container_info
-	    }))
-	.await
-
-	.unwrap();
-
-
-    let containers_response = containers_array
-        .iter()
-        .map(|container| {
-
-            let container_id = container["Id"].as_str().unwrap_or_default();
-
-	    let container_info = container;
-
-            (container_id.to_string(), container_info.clone())
-        })
-        .collect::<serde_json::Map<String, Value>>();
-
-    serde_json::to_value(containers_response).unwrap()
-}
-
 #[launch]
-async fn rocket() -> _ {
+async fn rocket() -> Rocket<Build> {
     let data: Data = Data {
-	docker: Client::unix()
+	docker: Client::unix(),
+	docker_socket: "/var/run/docker.sock"
     };
-
-
-    rocket::build().manage(data).mount("/", routes![v1_system_containers])
+    let rocket = rocket::build();
+    let rocket = routes::routes(rocket, "/");
+    let rocket = rocket.manage(data);
+    rocket
 }
